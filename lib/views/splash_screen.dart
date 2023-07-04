31bde76dd0d5f36da09dc7eac813685ec7b4c9e7
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2023-07-04 13:33:47 +0100

    added splash screen

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:innove_gen_ai_frontend/util/decoration_util.dart';
+import 'package:innove_gen_ai_frontend/views/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with DecorationUtil {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => withScreenDecoration(const Home()),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(46)),
+          ),
+          Center(
+            child: Text(
+              'Reviews you can trust',
+              style: prettifyText(Theme.of(context).textTheme.headlineMedium!),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
